dc4f898a820da51ca354fd01bd840f773379189b
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-12-07 17:28:48 -0600

    Install and initialize loaders

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./App.css"
+
+const App = () => {
+
+  return(
+    <h1 className="funky">testing</h1>
+  )
+
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from "./App.js"
 ReactDOM.render(
-    <h1>Hello World</h1>,
+    <App/>,
   document.getElementById('root')
 );
 
