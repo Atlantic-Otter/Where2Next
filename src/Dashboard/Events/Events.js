01ffc6623cc07035dac63c9ac5bbeccdedfc2ba2
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-12-07 20:54:16 -0800

    set up rudimentary route for rendering events on mount

--- src/Dashboard/Events/Events.js
+++ src/Dashboard/Events/Events.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import useSearchParams from "../../../Helpers/useSearchParams";
+import axios from "axios";
 function Events() {
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/nearbyEvents").then(({ data }) => {
+      setEvents(data);
+    });
+  }, []);
+
+  const eventList = events.map((event) => {
+    console.log(event);
+    return (
+      <>
+        <span>{event.name}</span> <br />
+      </>
+    );
+  });
   const { startDate, endDate, zip } = useSearchParams();
   console.log(startDate, endDate, zip);
-  return <div>Events</div>;
+  return (
+    <>
+      <div>Events</div> {eventList}
+    </>
+  );
 }
 
 export default Events;
