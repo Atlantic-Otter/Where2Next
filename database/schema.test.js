37b20a89447837c012d870c7b221db2ccb991534
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-12-08 02:23:39 -0600

    Add teardown

--- database/schema.test.js
+++ database/schema.test.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const User = require('./schema.js');
 
 describe('Initial schema tests', function() {
+  afterAll(function() {
+    // TODO: add a User.remove(//some function to delete documents added from the test, perhaps add a `created_at: Date.now()` to schema - at end of test call .remove on any documents matching (Date.now() - 3000)
+    mongoose.connection.close();
+  });
+
   test('should store user information', async function() {
     await User.create({
       username: 'Ellito',
@@ -16,7 +21,6 @@ describe('Initial schema tests', function() {
     })
     .catch((err) => {
       console.warn('testing error');
-
       throw err;
     })
   });
@@ -57,27 +61,22 @@ describe('Initial schema tests', function() {
       })
       .catch((err) => {
         console.warn('testing error');
-        mongoose.connection.close();
         throw err;
       });
+  });
 
-  })
-
-  test('should reject users with duplicate usernames', async function() {
+  test('should reject duplicate usernames', async function() {
     await expect(User.create({
       useraname: 'AdamJ',
       password: 'HiFromSF'
     })).rejects.toThrow('User validation failed:');
-
   });
 
-  test('should reject users with duplicate passwords', async function() {
+  test('should reject duplicate passwords', async function() {
     await expect(User.create({
       useraname: 'Charles_Arduino_weaponry',
       password: 'Bees'
     })).rejects.toThrow('User validation failed');
-    mongoose.connection.close();
-
   });
 
   test('should reject insert queries missing the required fields', async function() {
@@ -85,5 +84,5 @@ describe('Initial schema tests', function() {
   })
 });
 
-// todo: add a User.remove(//some function to delete users added from the test, perhaps add a `created_at: Date.now()` to schema - at end of test call .remove on any documents matching Date.now() - 3000);
+
 
