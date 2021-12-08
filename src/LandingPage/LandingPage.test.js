8ad83f08d8cea13cf706b5d7971d7661868a2174
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-12-07 18:55:14 -0800

    added simple LandingPage test

---
+++ src/LandingPage/LandingPage.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import LandingPage from "./LandingPage.js";
+import "@testing-library/jest-dom";
+import { HashRouter as Router, MemoryRouter } from "react-router-dom";
+
+const customRender = (ui) => {
+  return render(<Router>{ui}</Router>);
+};
+test("Renders the App Component on the page", () => {
+  customRender(<LandingPage />);
+
+  expect(screen.getByText("HOME")).toBeInTheDocument();
+});
