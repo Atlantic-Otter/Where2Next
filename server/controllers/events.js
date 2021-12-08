ee5956e6aa6c2dc2a6b72b56717c6471e04c494d
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-12-07 18:37:20 -0800

    added catch-all route for dashboard/events

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -1,18 +1,19 @@
-const axios = require('axios')
-const { API_KEY } = require('../../config.js')
+const axios = require("axios");
+const { API_KEY } = require("../../config.js");
 
-const URL = "https://api.predicthq.com/v1/events/?within=10km@37.804363,-122.271111"
+const URL =
+  "https://api.predicthq.com/v1/events/?within=10km@37.804363,-122.271111";
 const HEADERS = {
-      headers: {
-        Authorization: API_KEY
-      }
-    }
+  headers: {
+    Authorization: API_KEY,
+  },
+};
 
 module.exports = {
-
   getNearbyEvents: (req, res) => {
-    axios.get(URL, HEADERS )
-    .then( d=> console.log(d.data.results))
-  }
-
-}
+    axios.get(URL, HEADERS).then((d) => {
+      const { results } = d.data;
+      results.forEach((result) => console.log(result.entities));
+    });
+  },
+};
--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import {Routes, Route, Link} from "react-router-dom"
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 function Dashboard() {
   return (
     <div id="dashboardContainer">
@@ -8,12 +8,13 @@ function Dashboard() {
       <Link to="flights">Flights</Link>
       <Link to="hotels">Hotels</Link>
       <Routes>
-        <Route path="/events" element={<h1>Events</h1>}/>
-        <Route path="/flights" element={<h1>Flights</h1>}/>
-        <Route path="/hotels" element={<h1>Hotels</h1>}/>
+        <Route path="/" element={<h1>Events</h1>} />
+        <Route path="/events" element={<h1>Events</h1>} />
+        <Route path="/flights" element={<h1>Flights</h1>} />
+        <Route path="/hotels" element={<h1>Hotels</h1>} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
