cbae2c77be37f3d7bdaec549bd55ce3450a88ff6
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-12-07 19:27:35 -0800

    switched to TicketmasterAPI

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -1,19 +1,15 @@
 const axios = require("axios");
 const { API_KEY } = require("../../config.js");
 
-const URL =
-  "https://api.predicthq.com/v1/events/?within=10km@37.804363,-122.271111";
-const HEADERS = {
-  headers: {
-    Authorization: API_KEY,
-  },
-};
+const URL = `https://app.ticketmaster.com/discovery/v2/events.json?latlong=37.804363,-122.271111&radius=10&unit=miles&apikey=${API_KEY}`;
 
 module.exports = {
   getNearbyEvents: (req, res) => {
-    axios.get(URL, HEADERS).then((d) => {
-      const { results } = d.data;
-      results.forEach((result) => console.log(result.entities));
+    axios.get(URL).then((d) => {
+      // console.log(d.data);
+      const { events } = d.data._embedded;
+      // console.log(events);
+      events.forEach((result) => console.log(result._embedded.venues));
     });
   },
 };
