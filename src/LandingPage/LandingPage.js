265d859311a8a4183cd258c8f3e19984a74880bc
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-12-09 13:50:01 -0800

    basic error handling for landing page inputs

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -3,15 +3,25 @@ import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
   const navigate = useNavigate();
+  const startMin = new Date().toISOString().split("T")[0];
+  const [errorMessage, setErrorMessage] = useState("");
   const [formData, setFormData] = useState({
     city: "",
     state: "",
-    startDate: null,
-    endDate: null,
+    startDate: startMin,
+    endDate: startMin,
   });
-
+  console.log(formData);
   const submitSearch = () => {
     const { city, state, startDate, endDate } = formData;
+
+    if (!city || !state) {
+      return setErrorMessage("Please select a city and state");
+    }
+
+    if (!startDate || !endDate) {
+      return setErrorMessage("Please select a start and end date");
+    }
     navigate(
       `/dashboard/?city=${city}&state=${state}&startDate=${startDate}&endDate=${endDate}`
     );
@@ -21,6 +31,7 @@ function LandingPage() {
     newFormData[e.target.name] = e.target.value;
     setFormData(newFormData);
   };
+
   return (
     <div id="landingPage">
       <h1>HOME</h1>
@@ -29,9 +40,21 @@ function LandingPage() {
       <label htmlFor="state">State</label>
       <input type="text" name="state" onChange={onChange} />
       <label htmlFor="startDate">Start Date</label>
-      <input type="date" name="startDate" onChange={onChange} />
+      <input
+        type="date"
+        name="startDate"
+        min={startMin}
+        value={formData.startDate}
+        onChange={onChange}
+      />
       <label htmlFor="endDate">End Date</label>
-      <input type="date" name="endDate" onChange={onChange} />
+      <input
+        type="date"
+        name="endDate"
+        min={formData.startDate}
+        value={formData.endDate}
+        onChange={onChange}
+      />
       <button onClick={submitSearch}>Go!</button>
     </div>
   );
