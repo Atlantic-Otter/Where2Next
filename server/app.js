6e8be2af1ec2cff484860a52d5021569f4381e96
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-12-09 09:23:35 -0800

    added express middleware

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const { events } = require("./controllers");
 const app = express();
 app.use(cors());
+app.use(express.static("dist"));
+app.use(express.json());
 
 app.get(
   "/nearbyEvents/:city/:state/:startDate/:endDate",
