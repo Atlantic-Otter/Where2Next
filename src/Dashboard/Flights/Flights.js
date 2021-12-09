a6ab268302406661ad8c50b9a71036889c350c99
Sara Schaeffer <sara.schaeffer617@gmail.com>
2021-12-09 12:40:45 -0800

    getting flights from API upon page load

--- src/Dashboard/Flights/Flights.js
+++ src/Dashboard/Flights/Flights.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Flights() {
+  const [flights, setFlights] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:3000/flights`
+      )
+      .then((flights) => {
+        setFlights(flights)
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return <div>Flights</div>;
 }
 
