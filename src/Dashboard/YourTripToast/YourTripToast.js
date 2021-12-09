2675bc77303518f6a24b563aa28cf8110ae80fce
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-12-09 12:47:48 -0800

    local storage checking, rendered current trip in toast

--- src/Dashboard/YourTripToast/YourTripToast.js
+++ src/Dashboard/YourTripToast/YourTripToast.js
@@ -1,31 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import Toast from "react-bootstrap/Toast";
 import ToastContainer from "react-bootstrap/ToastContainer";
 import Button from "react-bootstrap/Button";
+import TripContext from "../../TripContext";
 function YourTripToast({ tripToastVisible, toggleTripToast }) {
+  const { currentTrip } = useContext(TripContext);
+  const eventList = currentTrip.events.map((event) => event.name);
+  const flightList = currentTrip.flights.map((flight) => flight.name);
+  const hotelList = currentTrip.hotels.map((hotel) => hotel.name);
+
   return (
     <ToastContainer position="bottom-end">
       <Toast bg="light" show={tripToastVisible} onClose={toggleTripToast}>
         <Toast.Header>My Trip</Toast.Header>
         <Toast.Body>
-          Nulla exercitation do aute quis culpa id. Et labore minim irure sunt
-          adipisicing laboris. Minim proident consequat nulla magna magna ipsum
-          sint incididunt laborum mollit velit qui esse nostrud. Dolor fugiat
-          veniam nostrud pariatur cupidatat eu cupidatat ex ipsum nisi
-          consectetur et adipisicing. Nulla exercitation do aute quis culpa id.
-          Et labore minim irure sunt adipisicing laboris. Minim proident
-          consequat nulla magna magna ipsum sint incididunt laborum mollit velit
-          qui esse nostrud. Dolor fugiat veniam nostrud pariatur cupidatat eu
-          cupidatat ex ipsum nisi consectetur et adipisicing. Nulla exercitation
-          do aute quis culpa id. Et labore minim irure sunt adipisicing laboris.
-          Minim proident consequat nulla magna magna ipsum sint incididunt
-          laborum mollit velit qui esse nostrud. Dolor fugiat veniam nostrud
-          pariatur cupidatat eu cupidatat ex ipsum nisi consectetur et
-          adipisicing. Nulla exercitation do aute quis culpa id. Et labore minim
-          irure sunt adipisicing laboris. Minim proident consequat nulla magna
-          magna ipsum sint incididunt laborum mollit velit qui esse nostrud.
-          Dolor fugiat veniam nostrud pariatur cupidatat eu cupidatat ex ipsum
-          nisi consectetur et adipisicing.
+          {eventList}
+          {flightList}
+          {hotelList}
         </Toast.Body>
         {/* <Button>Check Out!</Button> */}
       </Toast>
