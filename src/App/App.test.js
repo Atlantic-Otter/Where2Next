2ba9c4b28bf9101f3719549fdfc0a8fa49f2f2e0
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-12-09 13:20:43 -0800

    added test for yourtrip toast

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,37 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import App from "./App.js";
 import "@testing-library/jest-dom";
+import TripContext from "../TripContext";
+
+const customRender = (ui) => {
+  return render(
+    <TripContext.Provider
+      value={{
+        currentTrip: { events: [], hotels: [], flights: [] },
+        setCurrentTrip: () => {},
+      }}
+    >
+      {ui}
+    </TripContext.Provider>
+  );
+};
 test("Renders the App Component on the page", () => {
-  render(<App />);
+  customRender(<App />);
 
   expect(screen.getByText("HOME")).toBeInTheDocument();
+});
+
+test("Clicking 'Your Trip' button renders toast", async () => {
+  customRender(<App />);
+  fireEvent.click(screen.getByText("Go!"));
+  await waitFor(() => {
+    screen.getByText("your trip");
+  });
+  fireEvent.click(screen.getByText("your trip"));
+
+  await waitFor(() => {
+    screen.getByText("My Trip");
+  });
+  // expect(screen.getByText("HOME")).toBeInTheDocument();
 });
