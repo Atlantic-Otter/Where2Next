0c6fb238c1ae0a3d0fcccd567b0937b625fba86b
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-12-09 18:35:36 -0600

    User context

--- src/App/App.js
+++ src/App/App.js
@@ -6,8 +6,9 @@ import getTrip from "../../Helpers/getTrip.js";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "../Dashboard/Dashboard.js";
 import LandingPage from "../LandingPage/LandingPage.js";
-import TripContext from "../TripContext";
 import LoginButton from "../Login/LoginButton.js";
+import TripContext from "../TripContext";
+import UserContext from "../UserContext";
 
 const App = () => {
   ///// CLEAR STORAGE ON CHECKOUT
@@ -16,6 +17,9 @@ const App = () => {
     flights: [],
     hotels: [],
   });
+
+  const [user, setUser] = React.useState({});
+
   useEffect(() => {
     if (window.localStorage.getItem("currentTrip")) {
       setCurrentTrip(JSON.parse(window.localStorage.getItem("currentTrip")));
@@ -29,15 +33,17 @@ const App = () => {
   }, [currentTrip]);
 
   return (
-    <TripContext.Provider value={{ currentTrip, setCurrentTrip }}>
-      <LoginButton />
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/dashboard/*" element={<Dashboard />} />
-        </Routes>
-      </Router>
-    </TripContext.Provider>
+    <UserContext.Provider value={{ user, setUser}} >
+      <TripContext.Provider value={{ currentTrip, setCurrentTrip }}>
+        <LoginButton />
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/dashboard/*" element={<Dashboard />} />
+          </Routes>
+        </Router>
+      </TripContext.Provider>
+    </UserContext.Provider>
   );
 };
 
