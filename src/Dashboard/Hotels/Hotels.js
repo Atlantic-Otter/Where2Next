7db406dc476fe250a95d2fde652594d9c2f5ef02
Jake Borromeo <jakeb1050@gmail.com>
2021-12-10 11:09:15 -0700

    Revert "merging;"
    This reverts commit 25452c0c1820e78a2746dcc398a8fc2a1192dd63.

--- src/Dashboard/Hotels/Hotels.js
+++ src/Dashboard/Hotels/Hotels.js
@@ -1,39 +1,7 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
-import useSearchParams from "../../../Helpers/useSearchParams";
-import HotelListItem from './HotelListItem';
+import React from "react";
 
-const Hotels = (props) => {
-  const { startDate, endDate, city, state } = useSearchParams();
-  const [ hotelList, setHotelList ] = useState([]);
-
-  useEffect(() => {
-    fetchHotels()
-  }, [])
-
-  const fetchHotels = () => {
-    const url = `http://localhost:3000/hotels/${city}/${state}/${startDate}/${endDate}`;
-
-    axios.get(url)
-      .then((res) => {
-        setHotelList(res.data);
-        // console.log(res.data, 'hotels.js')
-      })
-      .catch((err) => {
-        console.log(err)
-      });
-    }
-
-  return (
-    <div>
-      {
-        hotelList.map((hotel, idx) =>
-          <HotelListItem
-            key={idx}
-            data={hotel}/>)
-      }
-    </div>
-  )
+function Hotels() {
+  return <div>Hotels</div>;
 }
 
 export default Hotels;
