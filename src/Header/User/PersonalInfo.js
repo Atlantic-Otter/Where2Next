ea93dfc1d9cdfefd2831191a2cebc761ae9668a8
Elliot Lichtenberg <elichtenberg2@gmail.com>
2021-12-15 19:24:43 -0600

    More styling

--- src/Header/User/PersonalInfo.js
+++ src/Header/User/PersonalInfo.js
@@ -3,15 +3,25 @@ import React from 'react';
 const PersonalInfo = ({ user }) => {
 
   return (
+    <>
+    <h4>My Info:</h4>
     <div id="user-info">
-      <h4>My Info:</h4>
-      <p>
-        Username: {user.username} <br />
-        Email: {user.email} <br />
-        Date Joined: {new Date(user.created_at).toLocaleDateString()}
-      </p>
+
+        <div className="user-info-row">
+          Username: <span className="user-info-value">{user.username}</span>
+        </div>
+
+        <div className="user-info-row">
+          Email: <span className="user-info-value">{user.email}</span>
+        </div>
+
+        <div className="user-info-row">
+          Date Joined: <span className="user-info-value">{new Date(user.created_at).toLocaleDateString()}</span>
+        </div>
+
 
     </div>
+    </>
   )
 
 };
