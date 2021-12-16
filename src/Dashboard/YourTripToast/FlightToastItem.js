9ed87d4665a349385785f9d8b08541710e734d79
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-12-16 11:39:55 -0800

    implemented "removeFlightFromTrip" function

--- src/Dashboard/YourTripToast/FlightToastItem.js
+++ src/Dashboard/YourTripToast/FlightToastItem.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import TripContext from "../../TripContext";
 
 function FlightToastItem({ flight }) {
+  console.log(flight);
   const { arrivalCode, departureCode } = flight;
+  const { currentTrip, setCurrentTrip } = useContext(TripContext);
+
+  const removeFlightFromTrip = () => {
+    const newTrip = { ...currentTrip };
+    newTrip.flights = newTrip.flights.filter((f) => f.id !== flight.id);
+
+    setCurrentTrip(newTrip);
+  };
+
   return (
     <div>
-      {departureCode}
-      {" to "}
-      {arrivalCode}.
+      <strong>
+        {departureCode}
+        {" to "}
+        {arrivalCode}
+      </strong>
+      {/* <span>{flight.quantity}</span> */}
+      <button onClick={removeFlightFromTrip}>REMOVE</button>
     </div>
   );
 }
