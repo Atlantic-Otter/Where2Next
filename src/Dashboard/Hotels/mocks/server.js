02e7c3b911470cb3fa317ab4b36f0546466e0e89
Jake Borromeo <jakeb1050@gmail.com>
2021-12-16 13:38:59 -0700

    Setup service worker

--- src/Dashboard/Hotels/mocks/server.js
+++ src/Dashboard/Hotels/mocks/server.js
@@ -1,4 +1,11 @@
 import {setupServer} from 'msw/node'
 import {handlers} from './handlers'
 
+// /handers:
+// /browser: sets up Service Worker
+// /mockServiceWorker: create with npx msw init public
+
+// /mocks/server: sets up server to use the same mocking logic in node
+// */setupTests: enables mocking for unit tests via beforeAll/ afterAll
+
 export const server = setupServer(...handlers)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./App/App.js"
+import App from "./App/App.js";
+
+if (process.env.NODE_ENV === 'development') {
+  require('./Dashboard/Hotels/mocks/browser')
+}
+
 ReactDOM.render(
     <App/>,
   document.getElementById('root')
