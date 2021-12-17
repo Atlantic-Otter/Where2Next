fbb7d73ddd12b8199378d039abd0a29e1d9c1440
Sara Schaeffer <sara.schaeffer617@gmail.com>
2021-12-17 12:30:58 -0800

    playing around with refactoring promises to async await

--- server/controllers/flights.js
+++ server/controllers/flights.js
@@ -5,15 +5,56 @@ const {
   FLIGHT_API_CLIENT_SECRET,
 } = require("../../config.js");
 
-module.exports = {
-  getFlights: (req, res) => {
+Controllers = {
+  // getFlights: (req, res) => {
+  //   const data = qs.stringify({
+  //     grant_type: "client_credentials",
+  //     client_id: FLIGHT_API_CLIENT_ID,
+  //     client_secret: FLIGHT_API_CLIENT_SECRET,
+  //   });
+  //   const { arrivalCode } = req.params;
+  //   console.log(arrivalCode);
+  //   const getTokenConfig = {
+  //     method: "post",
+  //     url: "https://test.api.amadeus.com/v1/security/oauth2/token",
+  //     headers: {
+  //       "Content-Type": "application/x-www-form-urlencoded",
+  //     },
+  //     data: data,
+  //   };
+
+  //   axios(getTokenConfig)
+  //     .then((tokenInfo) => {
+  //       const token = tokenInfo.data.access_token;
+  //       console.log(arrivalCode);
+  //       const getFlightsConfig = {
+  //         method: "get",
+  //         url: `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=JFK&destinationLocationCode=${arrivalCode}&departureDate=2021-12-20&adults=1`,
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       };
+  //       return axios(getFlightsConfig);
+  //     })
+  //     .then((flightInfo) => {
+  //       const flights = flightInfo.data.data;
+  //       res.status(200).send(flights);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //       res
+  //         .status(500)
+  //         .send("Something went wrong when generating an access token");
+  //     });
+  // },
+
+  getFlights: function(req, res) {
     const data = qs.stringify({
       grant_type: "client_credentials",
       client_id: FLIGHT_API_CLIENT_ID,
       client_secret: FLIGHT_API_CLIENT_SECRET,
     });
     const { arrivalCode } = req.params;
-    console.log(arrivalCode);
     const getTokenConfig = {
       method: "post",
       url: "https://test.api.amadeus.com/v1/security/oauth2/token",
@@ -22,29 +63,38 @@ module.exports = {
       },
       data: data,
     };
-
-    axios(getTokenConfig)
-      .then((tokenInfo) => {
-        const token = tokenInfo.data.access_token;
-        console.log(arrivalCode);
-        const getFlightsConfig = {
-          method: "get",
-          url: `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=JFK&destinationLocationCode=${arrivalCode}&departureDate=2021-12-20&adults=1`,
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        };
-        return axios(getFlightsConfig);
-      })
-      .then((flightInfo) => {
-        const flights = flightInfo.data.data;
-        res.status(200).send(flights);
-      })
-      .catch((error) => {
-        console.log(error);
-        res
-          .status(500)
-          .send("Something went wrong when generating an access token");
-      });
+    const token = Controllers.getToken(getTokenConfig);
+    const getFlightsConfig = {
+      method: "get",
+      url: `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=JFK&destinationLocationCode=${arrivalCode}&departureDate=2021-12-20&adults=1`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const flightData = Controllers.getFlightData(getFlightsConfig);
+    res.status(200).send(flightData);
   },
-};
+
+  getToken: async function(getTokenConfig) {
+    // try {
+      const token = await axios(getTokenConfig);
+      console.log('token:', token);
+      console.log('token access token:', token.data.access_token);
+      return token.data.access_token;
+    // } catch (error) {
+    //   console.log(error)
+    // }
+  },
+
+  getFlightData: async function(getFlightsConfig) {
+    // try {
+      const flightData = await axios(getFlightsConfig);
+      console.log('get flights config:', getFlightsConfig);
+      return flightData.data.data;
+    // } catch (error) {
+    //   console.log(error)
+    // }
+  }
+}
+
+module.exports = Controllers;
